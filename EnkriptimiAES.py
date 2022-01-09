5a513c19f988fee2bf9649cd3e6abeb809a33b4e
blerandrudhani <58183465+blerandrudhani@users.noreply.github.com>
2022-01-09 17:32:02 +0100

    Update EnkriptimiAES.py

--- EnkriptimiAES.py
+++ EnkriptimiAES.py
@@ -40,3 +40,26 @@ def put():
         ciphertext, tag = cipher.encrypt_and_digest(msg.encode("ascii"))
         return nonce, ciphertext, tag
 
+    
+    def decrypt(nonce, ciphertext, tag):
+        cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+        plaintext = cipher.decrypt(ciphertext)
+        try:
+            cipher.verify(tag)
+            return plaintext.decode('ascii')
+        except:
+            return False
+
+    nonce, ciphertext, tag = encrypt(mesazhi.get())
+    plaintext = decrypt(nonce, ciphertext, tag)
+
+    cipher.set(f'Mesazhi i enkriptuar: {ciphertext}')
+    if not plaintext:
+        plain.set("Mesazhi eshte bosh!")
+    else:
+        plain.set(f'Mesazhi i dekriptuar: {plaintext}')
+
+Button(text = "Enkripto", height="1", justify="center" , width = "10", command = put).pack()
+Label(screen, textvariable=plain).pack()
+Label(screen, textvariable=cipher).pack()
+screen.mainloop()
