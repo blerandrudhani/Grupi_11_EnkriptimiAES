a54d1e74aa4531cd071f165569ff82800aa07913
Ermirvishi <75865381+Ermirvishi@users.noreply.github.com>
2022-01-09 17:30:58 +0100

    Update EnkriptimiAES.py

--- EnkriptimiAES.py
+++ EnkriptimiAES.py
@@ -19,3 +19,24 @@ Radiobutton(screen, text="256 bit", variable = llojiEnc, value=32).pack()
 Label(text = "Mesazhi: ").pack()
 Entry(textvariable = mesazhi).pack()
 Label(text = "").pack()
+
+
+plain = StringVar()
+plain.set("")
+cipher = StringVar()
+cipher.set("")
+
+def selected():
+    return llojiEnc.get()
+
+def put():
+
+    key = token_bytes(int(selected()))
+
+
+    def encrypt(msg):
+        cipher = AES.new(key, AES.MODE_EAX)
+        nonce = cipher.nonce
+        ciphertext, tag = cipher.encrypt_and_digest(msg.encode("ascii"))
+        return nonce, ciphertext, tag
+
